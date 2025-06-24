d1293255b5f3c347d089ef1530a9c5ecca4c79c3
Sidharth K V <sidharthkv1100@gmail.com>
2025-06-24 19:33:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 const images = [
-  "7 series.jpg",
-  "a8.jpg",
-  "cullinan.jpg",
-  "s class.jpg"
+  "https://cdn.pixabay.com/photo/2016/11/22/19/22/bmw-1854734_1280.jpg",
+  "https://cdn.pixabay.com/photo/2020/02/17/17/41/bmw-4857036_1280.jpg",
+  "https://cdn.pixabay.com/photo/2016/01/19/17/46/bmw-1144911_1280.jpg",
+  "https://cdn.pixabay.com/photo/2020/02/10/19/33/bmw-4836245_1280.jpg",
+  "https://cdn.pixabay.com/photo/2018/06/23/20/16/car-3492930_1280.jpg"
 ];
 
 // Get the gallery container
 const gallery = document.getElementById("gallery");
 
 // Loop through the image list and create <img> elements
-images.forEach((imageName, index) => {
+images.forEach((url, index) => {
   const img = document.createElement("img");
-  img.src = `images/${imageName}`;
-  img.alt = `Photo ${index + 1}`;
+  img.src = url;
+  img.alt = `BMW Car ${index + 1}`;
   gallery.appendChild(img);
 });
