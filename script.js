32e7a38d4760612e58525b9143fb24efa04f1196
Sidharth K V <sidharthkv1100@gmail.com>
2025-06-24 20:18:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 const imageUrls = [
-  "https://images.unsplash.com/photo-1571607388263-6a8c2d43ec0f", // BMW front view
-  "https://images.unsplash.com/photo-1617531653332-98b1c2ebc0d1", // BMW black sports car
-  "https://images.unsplash.com/photo-1570129477492-45c003edd2be", // BMW i8
-  "https://images.unsplash.com/photo-1603712725036-bc59e8ec6633", // BMW interior
-  "https://images.unsplash.com/photo-1592194996308-7b43878e84a6", // BMW drifting
-  "https://upload.wikimedia.org/wikipedia/commons/1/1e/2019_BMW_M2_Competition_Automatic_3.0.jpg" // BMW M2
+  "https://picsum.photos/id/1015/400/300",
+  "https://picsum.photos/id/1016/400/300",
+  "https://picsum.photos/id/1020/400/300",
+  "https://picsum.photos/id/1024/400/300",
+  "https://picsum.photos/id/1027/400/300",
+  "https://picsum.photos/id/1035/400/300"
 ];
 
 const gallery = document.getElementById("gallery");
 
 imageUrls.forEach((url) => {
   const img = document.createElement("img");
-  img.src = `${url}?w=400&h=300&auto=format`; // Add size/format if from Unsplash
-  img.alt = "BMW Car";
+  img.src = url;
   gallery.appendChild(img);
 });
